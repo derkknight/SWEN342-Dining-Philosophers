557b455585304ec29a692d90877047282ab01b5f
niseguy <dmy4874@g.rit.edu>
2014-09-26 13:37:56 -0400

    Initial commit

---
+++ Dining Philosophers/src/Driver.java
@@ -0,0 +1,21 @@
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 1; i <= 4; i++)
+		{
+			if (true )
+			{
+				System.out.println("No negative numbers!");
+			}
+		}
+		
+		//Creates an array of np Forks
+		
+		
+		//np Philosopher threads are created, where Philospher's i right fork is i and left fork is (np + i - 1)%np
+		//note that i rnages from 0 to np - 1
+	}
+
+}
