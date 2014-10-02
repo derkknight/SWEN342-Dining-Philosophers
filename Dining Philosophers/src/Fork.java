c29d7002913562589b86b0a496cd98137cb21369
Aanarose42 <kms7491@kms7491s-MacBook-Pro.local>
2014-10-02 06:57:46 -0400

    try again for IFork and Fork

--- Dining Philosophers/src/Fork.java
+++ Dining Philosophers/src/Fork.java
@@ -1,4 +1,31 @@
 
-public class Fork {
+public class Fork implements IFork {
+	
+	private boolean available;
+	
+	public Fork() {
+		this.available = true;
+	}
+
+
+	public synchronized void acquire() {
+		while (this.available == false) {
+			try {
+				wait();
+			} catch (Exception e) {
+				System.err.println( "Abort. Unexpected thread interruption.");
+			}
+			
+		}
+		this.available = false;
+	}
+
+
+	public void release() {
+		this.available = true;
+		notifyAll();	
+	}
+	
+	
 
 }
---
+++ Dining Philosophers/src/IFork.java
@@ -0,0 +1,13 @@
+
+public interface IFork {
+	/*
+	 * A philosopher (attempts to) acquire the fork.
+	 */
+	public void acquire();
+	
+	/*
+	 * A philosopher releases the fork.
+	 */
+	public void release();
+
+}
