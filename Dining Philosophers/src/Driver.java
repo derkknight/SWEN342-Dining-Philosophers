03805dd3288358ebef1aaab17a869813a11581ae
niseguy <dmy4874@g.rit.edu>
2014-10-02 22:24:24 -0400

    Core functionality completed

--- Dining Philosophers/src/Driver.java
+++ Dining Philosophers/src/Driver.java
@@ -5,14 +5,15 @@ public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		boolean rhanded = true;
 		if (args.length <= 4)
 		{
-			int np;
+			long np;
 			int nt;
-			int tm;
-			int em;
+			long tm;
+			long em;
 			
-			for (int i = 0; i <= args.length; i++)
+			for (int i = 0; i < args.length; i++)
 			{
 				if (Integer.parseInt(args[i]) < 0)
 				{
@@ -24,44 +25,31 @@ public class Driver {
 			{
 				np = 4;
 				nt = Integer.parseInt(args[0]);
-				tm = Integer.parseInt(args[1]);
-				em = Integer.parseInt(args[2]);
+				tm = Long.parseLong(args[1]);
+				em = Long.parseLong(args[2]);
 			}
 			else
 			{
-				np = Integer.parseInt(args[0]);
+				np = Long.parseLong(args[0]);
 				nt = Integer.parseInt(args[1]);
-				tm = Integer.parseInt(args[2]);
-				em = Integer.parseInt(args[3]);
+				tm = Long.parseLong(args[2]);
+				em = Long.parseLong(args[3]);
 			}
 			
 			ArrayList<Fork> forks = new ArrayList<Fork>();
 			ArrayList<Philosopher> phils = new ArrayList<Philosopher>();
-			for (int i = 0; i < np - 1; i++)
+			for (int i = 0; i < np ; i++)
 			{
 				forks.add(new Fork());
 			}
-			for (int i = 0; i < np; i++)
+			for (int i = 0; i < np - 1; i++)
 			{
-				phils.add(new Philosopher());
-				//phils.add(new Philosopher(i, i, (np+i-1)%np,nt, tm, em));
+				int np_int = (int)np;
+				(new Philosopher(i, forks.get(i), forks.get((np_int+i-1)%np_int), rhanded, nt, tm, em)).start();
 			}
 
 		}
 		
-		for (int i = 1; i <= 4; i++)
-		{
-			if (Integer.parseInt(args[i]) < 0)
-			{
-				System.out.println("No negative numbers!");
-			}
-		}
-		
-		//Creates an array of np Forks
-		
-		
-		//np Philosopher threads are created, where Philospher's i right fork is i and left fork is (np + i - 1)%np
-		//note that i rnages from 0 to np - 1
 	}
 
 }
